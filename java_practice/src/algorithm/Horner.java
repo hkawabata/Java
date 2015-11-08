336f4f141adf53c3111ae76a05af8d6ebb8296ce
hkawabata <meiwaku_mail_iyaaaaa@yahoo.co.jp>
2015-11-08 18:43:55 +0900

    add test code to Horner

--- java_practice/src/algorithm/Horner.java
+++ java_practice/src/algorithm/Horner.java
@@ -5,6 +5,8 @@ package algorithm;
  */
 
 public class Horner {
+	
+	// Horner の方法
 	public static double polynomialHorner (double a[], double x) {
 		int n = a.length;
 		double ret = a[0];
@@ -12,5 +14,49 @@ public class Horner {
 			ret = ret * x + a[i];
 		}
 		return ret;
+	}
+	
+	// pow 関数を使って和を取る手法
+	public static double polynomial (double a[], double x) {
+		int n = a.length;
+		double ret = 0;
+		for (int i=0; i<n; i++) {
+			ret += a[i] * Math.pow(x, n-i-1);
+		}
+		return ret;
+	}
+	
+	/* 
+	 * テストコード
+	 */
+	
+	// 2手法の計算速度の違いを測定する
+	public static void polinomialTest () {
+		int arr_size = 65536;
+		int trial_time = 100;
+
+		double[] a = new double[arr_size];
+		for (int i=0; i<arr_size; i++) {
+			a[i] = (i + 1) * 1;
+		}
+		
+		long start, end;
+		double tmp;
+
+		System.out.println("### Horner's method ###");
+		start = System.currentTimeMillis();
+		for (int i=0; i<trial_time; i++) {
+			tmp = Horner.polynomialHorner(a, 0.1);
+		}
+		end = System.currentTimeMillis();
+		System.out.println("coluculation time:\t" + (end-start) + " ms.");
+		
+		System.out.println("### normal method ###");
+		start = System.currentTimeMillis();
+		for (int i=0; i<trial_time; i++) {
+			tmp = Horner.polynomial(a, 0.1);
+		}
+		end = System.currentTimeMillis();
+		System.out.println("coluculation time:\t" + (end-start) + " ms.");
 	}
 }
--- java_practice/src/main/Main.java
+++ java_practice/src/main/Main.java
@@ -4,7 +4,6 @@ import algorithm.Horner;
 
 public class Main {
 	public static void main (String[] args) {
-		double a[] = {1,2,3};
-		System.out.println(Horner.polynomialHorner(a, 10));
+		Horner.polinomialTest();
 	}
 }
