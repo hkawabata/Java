95219272aa26f4b4b8d3db80e966820d4afd9986
hkawabata <meiwaku_mail_iyaaaaa@yahoo.co.jp>
2015-12-05 13:01:35 +0900

    add TestClass LongNumberTest

---
+++ java_practice/src/test/java/algorithm/LongNumberTest.java
@@ -0,0 +1,20 @@
+package algorithm;
+
+import static org.junit.Assert.*;
+import org.junit.Test;
+
+import algorithm.LongNumber;
+
+public class LongNumberTest {
+
+	@Test
+	public void plusTest() {
+		assertEquals(1,1);
+	}
+	
+	@Test
+	public void minusTest() {
+		fail("Not yet implemented");
+	}
+
+}
