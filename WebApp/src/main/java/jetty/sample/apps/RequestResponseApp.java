73ea3392182bdc275da0f28fc665791ed3839550
batako-san <hkawabata_work@yahoo.co.jp>
2016-07-10 19:29:38 +0900

    impliment RequestResponseApp

--- WebApp/src/main/java/jetty/sample/apps/RequestResponseApp.java
+++ WebApp/src/main/java/jetty/sample/apps/RequestResponseApp.java
@@ -10,15 +10,10 @@ public class RequestResponseApp extends HttpServlet {
 
     public void doGet(HttpServletRequest request, HttpServletResponse response)
             throws IOException, ServletException{
-        response.setContentType("text/html; charset=UTF-8");
+        response.setContentType("application/json; charset=UTF-8");
         PrintWriter out = response.getWriter();
-        out.println("<html>");
-        out.println("<head>");
-        out.println("<title>Hello World!</title>");
-        out.println("</head>");
-        out.println("<body>");
-        out.println("<h1>Hello World!</h1>");
-        out.println("</body>");
-        out.println("</html>");
+        String id = request.getParameter("id");
+        String pass = request.getParameter("pass");
+        out.println(String.format("{\"id\": %s, \"pass\": \"%s\"}", id, pass));
     }
 }
