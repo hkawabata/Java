b0562402d0976e13e33025c053802bed1a844883
hkawabata <meiwaku_mail_iyaaaaa@yahoo.co.jp>
2017-01-30 21:40:50 +0900

    new class: FacebookAPI & TwitterAPI

---
+++ java_practice/src/main/java/jp/hkawabata/api/facebook/FacebookAPI.java
@@ -0,0 +1,12 @@
+package jp.hkawabata.api.facebook;
+
+/**
+ * Facebook の API を試す
+ *
+ * Created by kawabatahiroto on 2017/01/30.
+ */
+public class FacebookAPI {
+    public static void main(String[] args) {
+
+    }
+}
---
+++ java_practice/src/main/java/jp/hkawabata/api/twitter/TwitterAPI.java
@@ -0,0 +1,12 @@
+package jp.hkawabata.api.twitter;
+
+/**
+ * twitter の API を試す
+ *
+ * Created by kawabatahiroto on 2017/01/30.
+ */
+public class TwitterAPI {
+    public static void main(String[] args) {
+
+    }
+}
